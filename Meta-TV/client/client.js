e7c6749b117a3f177140ea13a1c50ade748451db
Andreas Linn <encrypt.wave@gmail.com>
2014-05-19 21:50:08 +0200

    Working version

--- Meta-TV/client/client.js
+++ Meta-TV/client/client.js
@@ -1,5 +1,27 @@
+slideshow = new Meteor.Collection("slideshow")
+
+var cursor = []
+
 Router.map(function() {
 	this.route('slideshow', {
-		path: '/'
+		path: '/',
+		data: function() {
+			update()
+		}
 	})
 })
+
+var timeout = 10 // s
+
+Template.slideshow.current = function() {
+	return Session.get("current")
+}
+
+
+function update() {
+	if(cursor.length === 0) {
+		cursor = slideshow.find().fetch()
+	}
+	Session.set("current", cursor.pop())
+}
+setInterval(update, timeout * 1000)
