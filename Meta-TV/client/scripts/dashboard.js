cced02baa2dc324990d41d468f891e32a22a9db7
sootn <encrypt.wave@gmail.com>
2014-09-15 16:28:09 +0200

    Fix

--- Meta-TV/client/scripts/dashboard.js
+++ Meta-TV/client/scripts/dashboard.js
@@ -1,22 +1,5 @@
 Template._loginButtonsMessages.infoMessage = "@kth.se mail required"
 
-var send_external_img = function(obj, report){
-	testImage($(".link").val(), function(url, result){
-		if(result=="success"){
-			obj.link = url
-			slideshow.insert(obj)
-			$(".link").val("")
-			report(true)
-		}else if (result=="error"){
-			var _id = Session.set("link_input_error", "(╯°Д°)╯ This is not an URL to a supported image format!!!")
-			report(false, _id)
-		}else if (result=="timeout"){
-			var _id = Session.set("internal_filetype_error", "ʕ๑◞◟๑ʔ Time out?!!")
-			report(false, _id)
-		}
-	})
-}
-
 Template.slide.events({
 	"click .remove": function() {
 		var obj_cp = {}
--- Meta-TV/client/scripts/uploader.js
+++ Meta-TV/client/scripts/uploader.js
@@ -2,6 +2,23 @@ Template.uploader.internal = function() {
 	return Session.get("type") == "local img"
 }
 
+var send_external_img = function(obj, report){
+	testImage($(".link").val(), function(url, result){
+		if(result=="success"){
+			obj.link = url
+			slideshow.insert(obj)
+			$(".link").val("")
+			report(true)
+		}else if (result=="error"){
+			var _id = Session.set("link_input_error", "(╯°Д°)╯ This is not an URL to a supported image format!!!")
+			report(false, _id)
+		}else if (result=="timeout"){
+			var _id = Session.set("internal_filetype_error", "ʕ๑◞◟๑ʔ Time out?!!")
+			report(false, _id)
+		}
+	})
+}
+
 Template.uploader.isYoutube = function() {
 	return Session.get("type") == "youtube"
 }
