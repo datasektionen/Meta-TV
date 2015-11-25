01dc2df119685ec6d8ac657166cad82dd9f802db
jsimo <simonsson.jesper@gmail.com>
2015-11-25 22:02:53 +0100

    tempfix for #58 for everyone as the previous hack 403s

--- server/auth.js
+++ server/auth.js
@@ -21,31 +21,15 @@ Accounts.registerLoginHandler("kth", function(loginRequest) {
 				token: loginRequest.token
 			})
 		} else {
-			url = "http://www.csc.kth.se/hacks/new/xfinger/results.php?freetext=" + usomething
-			Meteor.http.call("GET", url, function(err, data) {
-				if(err) {
-					console.error(err)
-					future.return(undefined)
-					return
-				}
-				if(data.content.match(/Inget resultat/)) {
-					var userId = Meteor.users.insert({
-						username: usomething,
-						usomething: usomething
-					});
-					console.log("xfinger did not find the user, created usomething users insted", usomething);
-				} else {
-					var username = data.content.match(/mailMe\('(\w+)/)[1]
-					var userId = Meteor.users.insert({
-						username: username,
-						usomething: usomething
-					})
-				}
-				console.log("created", userId)
-				future.return({
-					userId: userId,
-					token: loginRequest.token
-				})
+			var userId = Meteor.users.insert({
+				username: usomething,
+				usomething: usomething
+			});
+	
+			console.log("created", userId)
+			future.return({
+				userId: userId,
+				token: loginRequest.token
 			})
 		}
 	})
