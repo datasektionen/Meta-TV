355e08aca4480755faa8ffb75e1ce3e65f5d0deb
Jesper Simonsson <simonsson.jesper@gmail.com>
2016-03-31 17:01:38 +0200

    fixed history bug

--- client/scripts/page.js
+++ client/scripts/page.js
@@ -6,7 +6,17 @@ Template.page.events({
 		Meteor.call("setScreen", this._id, selected_screen)
 	},
 
-	"click .remove":function(event) {
+	"click .removepage":function(event) {
+		var obj_cp = {}
+		shallow_copy(obj_cp, this)
+		history_log.insert({
+			action: "Removed page",
+			by: Meteor.user().username,
+			time: Date.now(),
+			parentid: this._id,
+			name: this.name,
+			obj: obj_cp,
+		})
 		Meteor.call("removePage", this._id)
 	}
 })
