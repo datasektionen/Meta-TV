df739c4e088e32c0036aa6147b575b343f3adfc9
mattiasgrenfeldt <9297883+mattiasgrenfeldt@users.noreply.github.com>
2020-11-28 23:28:25 +0100

    Replaced use of login2.datasektionen.se with login.datasektionen.se

--- server/auth.js
+++ server/auth.js
@@ -3,8 +3,8 @@ var Future = Npm.require('fibers/future')
 
 Accounts.registerLoginHandler("kth", function(loginRequest) {
 	var future = new Future
-	var url = "http://login2.datasektionen.se/verify/" + loginRequest.token + ".json"
-	Meteor.http.call("GET", url, {params: {api_key: process.env.LOGIN2_KEY}} ,function(err, data) {
+	var url = "https://login.datasektionen.se/verify/" + loginRequest.token + ".json"
+	Meteor.https.call("GET", url, {params: {api_key: process.env.LOGIN_KEY}} ,function(err, data) {
 		if(err) {
 			console.error(err)
 			future.return(undefined)
